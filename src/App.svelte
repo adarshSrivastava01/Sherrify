34f43d775ad2a3a4e1bc4a9f198325c0f6551730
adarshSrivastava01 <59641575+adarshSrivastava01@users.noreply.github.com>
2021-10-11 20:26:56 +0530

    Happy Coding 🚀

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let name = 'Adarsh';
+</script>
+
+<main>Adarsh {name}!</main>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
